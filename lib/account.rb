93893e7518571d86bbf6c958335acedcdb3c76e5
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-06-12 13:07:21 +0100

    Add deposit and withdraw methods to Account

--- lib/account.rb
+++ lib/account.rb
@@ -1,3 +1,10 @@
 class Account
   attr_reader :balance
+
+  def deposit
+  end
+
+  def withdraw
+  end
+  
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,4 +1,6 @@
 describe Account do
   alias_method :account, :subject
   it { is_expected.to respond_to(:balance) }
+  it { is_expected.to respond_to(:deposit) }
+  it { is_expected.to respond_to(:withdraw) }
 end
