8971d6e84efe016ba9951bde5d7d39e826844ea5
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-06-12 13:40:27 +0100

    Account has transactions attribute

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,6 +1,7 @@
 describe Account do
   alias_method :account, :subject
   it { is_expected.to respond_to(:balance) }
+  it { is_expected.to respond_to(:transactions) }
 
   it 'deposit specified amount to account balance' do
     expect { account.deposit(12) }.to change { account.balance }.by 12
